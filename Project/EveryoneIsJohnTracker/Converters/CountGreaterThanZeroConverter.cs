faf2ce3c0f910a6595c5ac5d8f5a35a2ed335104
Phillip <Filuperwinsmith@gmail.com>
2019-12-14 18:44:48 +1100

    Adding Voice Editing

---
+++ Project/EveryoneIsJohnTracker/Converters/CountGreaterThanZeroConverter.cs
@@ -0,0 +1,37 @@
+﻿#region Title Header
+
+// Name: Phillip Smith
+// 
+// Solution: EveryoneIsJohnTracker
+// Project: EveryoneIsJohnTracker
+// File Name: CountGreaterThanZeroConverter.cs
+// 
+// Current Data:
+// 2019-12-14 4:33 PM
+// 
+// Creation Date:
+// 2019-12-14 4:28 PM
+
+#endregion
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace EveryoneIsJohnTracker.Converters
+{
+internal class CountGreaterThanZeroConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var count = System.Convert.ToInt32(value);
+
+        return count > 0;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+}
