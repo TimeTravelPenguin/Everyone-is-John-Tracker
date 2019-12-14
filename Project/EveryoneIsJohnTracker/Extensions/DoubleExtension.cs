b6e3e28cee07210f810e9f5153f2bb18e577783b
Phillip <Filuperwinsmith@gmail.com>
2019-12-14 11:14:58 +1100

    Added NotEqual

--- Project/EveryoneIsJohnTracker/Extensions/DoubleExtension.cs
+++ Project/EveryoneIsJohnTracker/Extensions/DoubleExtension.cs
@@ -7,7 +7,7 @@
 // File Name: DoubleExtension.cs
 // 
 // Current Data:
-// 2019-12-13 5:02 PM
+// 2019-12-14 10:48 AM
 // 
 // Creation Date:
 // 2019-09-28 4:35 PM
@@ -32,7 +32,12 @@ namespace EveryoneIsJohnTracker.Extensions
 
         public static bool IsGreaterThanZero(this double lhs)
         {
-            return Math.Abs(lhs) > double.Epsilon;
+            return Math.Abs(lhs) > 0;
+        }
+
+        public static bool NotEqual(this double lhs, double rhs)
+        {
+            return Math.Abs(lhs - rhs) > double.Epsilon;
         }
     }
 }
