571b03f07cf5a02682c62775ce4ef2b2da18c504
Phillip Smith <FilupErwinSmith@gmail.com>
2019-12-18 12:54:47 +1100

    Created new converter

---
+++ Project/EveryoneIsJohnTracker/Types/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,53 @@
+﻿#region Title Header
+
+// Name: Phillip Smith
+// 
+// Solution: EveryoneIsJohnTracker
+// Project: EveryoneIsJohnTracker
+// File Name: InverseBooleanConverter.cs
+// 
+// Current Data:
+// 2019-12-18 11:53 AM
+// 
+// Creation Date:
+// 2019-12-18 11:53 AM
+
+#endregion
+
+using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Windows.Data;
+
+namespace EveryoneIsJohnTracker.Types.Converters
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InverseBooleanConverter : IValueConverter
+    {
+        private readonly ResourceManager _resourceManager =
+            new ResourceManager("DnDTracker.Resources.Exceptions.ExceptionMessages", Assembly.GetExecutingAssembly());
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+            {
+                throw new InvalidOperationException(_resourceManager.GetString("TargetMustBeBool",
+                    CultureInfo.InvariantCulture));
+            }
+
+            if (value == null)
+            {
+                throw new InvalidOperationException(_resourceManager.GetString("CannotBeNull",
+                    CultureInfo.InvariantCulture));
+            }
+
+            return !(bool) value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
