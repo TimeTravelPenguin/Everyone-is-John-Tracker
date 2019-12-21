f4516c02abccdf1211fa5d767e93f9ae4df143f4
Phillip Smith <FilupErwinSmith@gmail.com>
2019-12-22 10:58:49 +1100

    Added Exception Messages resource

--- Project/EveryoneIsJohnTracker/Resources/Exceptions/ExceptionMessages.Designer.cs
+++ Project/EveryoneIsJohnTracker/Resources/Exceptions/ExceptionMessages.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace EveryoneIsJohnTracker.Properties {
+namespace EveryoneIsJohnTracker.Resources.Exceptions {
     using System;
     
     
@@ -22,14 +22,14 @@ namespace EveryoneIsJohnTracker.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    internal class ExceptionMessages {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources() {
+        internal ExceptionMessages() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace EveryoneIsJohnTracker.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EveryoneIsJohnTracker.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EveryoneIsJohnTracker.Resources.Exceptions.ExceptionMessages", typeof(ExceptionMessages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -57,6 +57,24 @@ namespace EveryoneIsJohnTracker.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The value cannot be null.
+        /// </summary>
+        internal static string CannotBeNull {
+            get {
+                return ResourceManager.GetString("CannotBeNull", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The target value must be of type bool.
+        /// </summary>
+        internal static string TargetMustBeBool {
+            get {
+                return ResourceManager.GetString("TargetMustBeBool", resourceCulture);
             }
         }
     }
